2bd85f9b195f12b6aa55080dd29f9ea24fa7d926
navtesh00 <navtesh00@users.noreply.github.com>
2026-09-05 09:23:05 +0530

    add .gitignore

--- node_modules/agentcart-server/start-server.js
+++
@@ -1,5 +0,0 @@
-import { default as app, initDB } from './src/index.js';
-
-await initDB();
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`AgentCart server listening http://localhost:${PORT}`));
--- server/start-server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\AgentCart\agentcart\server
-node src/index.js
